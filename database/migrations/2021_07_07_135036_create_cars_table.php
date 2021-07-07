384785c0e5c69ef130e3bfb2fcc66444a0e4d346
gaiyadev <gaiyaobed94@gmail.com>
2021-07-07 14:59:28 +0100

    Migration file for carController and c

--- database/migrations/2021_07_07_135036_create_cars_table.php
+++ database/migrations/2021_07_07_135036_create_cars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminsTable extends Migration
+class CreateCarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique()->notNullable();
-            $table->string('password');
+            $table->string('symptoms');
+            $table->string('causes');
+            $table->string('solution');
+            $table->string('carType');
+            $table->string('yearOfManufacture');
+            $table->string('type');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
@@ -29,6 +33,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('cars');
     }
 }
